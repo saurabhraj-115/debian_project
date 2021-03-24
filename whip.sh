e7a7c303b0db98e9b0df7a7baa5aa437c304ebef
Saurabh Raj <saurabhraj115@gmail.com>
2021-03-24 08:04:55 -0400

    with binaries

--- whip.sh
+++ whip.sh
@@ -2,10 +2,12 @@
 
 ar x openocd-all.deb
 tar -xvf data.tar.xz
-cd code
-sudo ./bootstrap
-sudo ./configure --enable-maintainer-mode --disable-werror --enable-ft2232_libftdi
-sudo make
-sudo make install
+#cd code
+#sudo ./bootstrap
+#sudo ./configure --enable-maintainer-mode --disable-werror --enable-ft2232_libftdi
+#sudo make
+#sudo make install
 
-exit 
+cp openocd /usr/local/bin
+
+exit
