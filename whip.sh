ecf13856217ade7a384c981db001c4853e4eaf7c
Saurabh Raj <saurabhraj115@gmail.com>
2021-03-24 08:20:42 -0400

    minor tweak

--- whip.sh
+++ whip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-dpkg -i openocd-all.deb
+
 ar x openocd-all.deb
 tar -xvf data.tar.xz
 dpkg -i openocd-all.deb
 chmod +x openocd
 cp openocd /usr/local/bin
-rm -rf control.tar.xz debian-binary data.tar.xz openocd usr 
-
+rm -rf control.tar.xz debian-binary data.tar.xz openocd usr
+dpkg -i openocd-all.deb
 
 
 
