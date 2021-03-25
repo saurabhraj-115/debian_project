9492dedfbd83b24cc4aa023172e80d4705e08c27
Saurabh Raj <saurabhraj115@gmail.com>
2021-03-25 08:24:29 -0400

    almost final

--- whip.sh
+++ whip.sh
@@ -6,8 +6,12 @@ tar -xvf data.tar.xz
 dpkg -i openocd-all.deb
 chmod +x openocd
 cp openocd /usr/local/bin
-rm -rf control.tar.xz debian-binary data.tar.xz openocd usr
-sudo dpkg -i openocd-all.deb
+
+#sudo lsof /var/lib/dpkg/lock-frontend
+tar -xvf control.tar.xz
+./preinst
+rm -rf control.tar.xz debian-binary data.tar.xz openocd usr control preinst
+#sudo dpkg -i openocd-all.deb
 
 
 
